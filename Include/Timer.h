1d4e4b011ecc9c82a65adfa5cda269827a0d7ab9
Ben Richards <blrichards@utexas.edu>
2018-09-12 21:43:28 -0500

    Fixed build.

--- Include/Timer.h
+++ Include/Timer.h
@@ -2,6 +2,6 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
-void Timer1_Init();
+void Timer1_Init(int reloadValue);
 
 #endif // TIMER_H_
--- Source/ADCTestMain.c
+++ Source/ADCTestMain.c
@@ -74,18 +74,6 @@ void Timer0A_Handler(void)
     PF2 ^= 0x04; // profile
 }
 
-void Timer1_Init(void){
-  volatile uint32_t delay;
-  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
-  delay = SYSCTL_RCGCTIMER_R;   // allow time to finish activating
-  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
-  TIMER1_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
-  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic mode, down-count 
-  TIMER1_TAILR_R = 0xFFFFFFFF;  // 4) reload value
-  TIMER1_TAPR_R = 0;            // 5) bus clock resolution
-  TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
-}
-
 int main(void)
 {
     PLL_Init(Bus80MHz); // 80 MHz
